7ca84eddb9034076eb726858dfe58d7747a5857d
scvrylullaby <vadimzooru@gmail.com>
2026-06-19 17:12:39 +0800

    fix: lock sidebar items for non auth users

--- src/components/layout/sidebar/ui/sidebar-item.tsx
+++ src/components/layout/sidebar/ui/sidebar-item.tsx
@@ -4,6 +4,7 @@ import { cn } from '@/libs/tw-merge'
 import { SidebarRoute } from '../model/sidebar-route.model'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useAuth } from '@/hooks/useAuth'
 
 interface Props {
   route: SidebarRoute
@@ -11,6 +12,12 @@ interface Props {
 
 export function SidebarItem({ route }: Props) {
   const pathname = usePathname()
+  const { isAuthenticated } = useAuth()
+  if (
+    !isAuthenticated &&
+    (route.label === 'Профиль' || route.label === 'Мои ставки')
+  )
+    return
   const isActive = pathname.includes(route.href)
   return (
     <Link
@@ -19,18 +26,22 @@ export function SidebarItem({ route }: Props) {
       className={cn(
         'flex h-[102.86px] w-full flex-col items-center justify-center gap-3 rounded-[var(--radius-xs)] border border-transparent px-3 py-2',
         'text-[var(--neutral-0)] transition-all hover:border-[var(--brand-200)]',
-        isActive && ['border-transparent text-[var(--brand-200)] hover:border-transparent']
+        isActive && [
+          'border-transparent text-[var(--brand-200)] hover:border-transparent'
+        ]
       )}
     >
-      <route.icon className={cn(
-        'size-6 shrink-0',
-        isActive && 'drop-shadow-[0_0_12px_rgba(228,189,78,0.5)]'
-      )}
+      <route.icon
+        className={cn(
+          'size-6 shrink-0',
+          isActive && 'drop-shadow-[0_0_12px_rgba(228,189,78,0.5)]'
+        )}
       />
-      <span className={cn(
-        'w-full text-center typo-xsmall',
-        isActive && 'text-shadow-gold'
-      )}
+      <span
+        className={cn(
+          'w-full text-center typo-xsmall',
+          isActive && 'text-shadow-gold'
+        )}
       >
         {route.label}
       </span>
